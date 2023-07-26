59b9684eeaf747d36a22593993b3f9bc472ae643
Kristinaliebhart <kristinalie@edu.aau.at>
2023-07-26 11:31:12 +0200

    refactor

--- Support.js
+++ Support.js
@@ -1,5 +1,5 @@
 function mm2px(valueMM){
-  const ppi = window.devicePixelRatio * 264; // PPI des iPad Pro 12,9 Zoll (5. Generation)
+  const ppi =  264; // PPI des iPad Pro 12,9 Zoll (5. Generation)
   // Convert mm to pixels
   const valuePx = valueMM * (ppi / 25.4);
   return valuePx;
