1a172d52f4364533aa5e359c5ec34c4fc5e0052a
Kristinaliebhart <kristinalie@edu.aau.at>
2023-07-26 09:44:53 +0200

    resolution and ppi for ipad pro

--- Support.js
+++ Support.js
@@ -9,16 +9,22 @@ function mm2px(valueMM){
 //resolution: 2048x2732
 //inches: 12.9
 
-/*  const ppi = 265; // set the dpi to 265
+
+//IPAD PRO
+const ppi = 265; // set the dpi to 265
   const inches = valueMM / 25.4; // convert mm to inches
   const valuePx = inches * ppi * window.devicePixelRatio; // calculate pixels
   return valuePx;
-  */
+  
 
-  const ppi = window.devicePixelRatio * 96; // assuming a default DPI of 96
+
+//Laptop
+ /* const ppi = window.devicePixelRatio * 96; // assuming a default DPI of 96
   // Convert  mm to pixels
   const valuePx = valueMM * (ppi / 25.4);
   return valuePx;
+
+  */
 }
 
 //test
